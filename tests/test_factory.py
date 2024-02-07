af4247312a540a2f25bc4a0bf465d8805dc67287
OfcPeriwinkle <ajwald@ucdavis.edu>
2024-02-07 12:52:04 -0800

    monkeypatch FLASK_ENV var for normal app test

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,16 +1,18 @@
+import pytest
 from flask.testing import FlaskClient
 
 import open_trs
 import open_trs.configs
 
 
-def test_config():
-    normal_app = open_trs.create_app()
-    assert not normal_app.testing
-
+def test_config(monkeypatch: pytest.MonkeyPatch):
     testing_app = open_trs.create_app(testing=True)
     assert testing_app.testing
     assert testing_app.config['DATABASE'] == open_trs.configs.TestingConfig.DATABASE
+
+    monkeypatch.setenv('FLASK_ENV', 'development')
+    normal_app = open_trs.create_app()
+    assert not normal_app.testing
 
 
 def test_hello(client: FlaskClient):
