43f815ec6d29e8a9ed697fc22057eb45c51a57bd
OfcPeriwinkle <ajwald@ucdavis.edu>
2024-02-07 12:13:47 -0800

    add test for succefully registering a user; add tests for validating input

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,21 +1,49 @@
-import json
-
 import pytest
 from flask import Flask
 from flask.testing import FlaskClient
 
+import open_trs.db
 
-def test_register_already_existing(client: FlaskClient):
+
+def test_register_new_user(client: FlaskClient, app: Flask):
+    new_user_info = {
+        'username': 'new_user',
+        'email': 'new_user@email.com',
+        'password': 'new_user_password'}
+
     response = client.post(
         '/auth/register',
         headers={'Content-Type': 'application/json'},
-        data=json.dumps({
-            'username': 'test',
-            'email': 'test@test.com',
-            'password': 'test'}))
+        json=new_user_info)
+
+    assert response.status_code == 201
+
+    success_message = response.get_json().get('message')
+    assert success_message is not None
+    assert 'registered successfully' in success_message
+
+    with app.app_context():
+        db = open_trs.db.get_db()
+        users = db.execute('SELECT * FROM Users WHERE username = ?', ('new_user',)).fetchall()
+
+        assert len(users) == 1
+        assert users[0]['username'] == new_user_info['username']
+        assert users[0]['email'] == new_user_info['email']
+        assert users[0]['password'] != new_user_info['password']
+
+
+@pytest.mark.parametrize(('username', 'email', 'password', 'message'), (
+    ('', '', '', b'Username is required'),
+    ('test', '', '', b'Email is required'),
+    ('test', 'test@test.com', '', b'Password is required'),
+    ('test', 'test@test.com', 'test',
+     b'Either user \\"test\\" or email \\"test@test.com\\" is already registered.')
+))
+def test_register_validate_input(client: FlaskClient, username, email, password, message):
+    response = client.post(
+        '/auth/register',
+        headers={'Content-Type': 'application/json'},
+        json={'username': username, 'email': email, 'password': password})
 
     assert response.status_code == 400
-
-    error_message = response.get_json().get('error')
-    assert error_message is not None
-    assert 'is already registered' in error_message
+    assert message in response.data
