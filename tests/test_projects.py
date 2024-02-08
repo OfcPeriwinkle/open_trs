93a5ea729da640cf63bae07677f80a023dcbc2f7
OfcPeriwinkle <ajwald@ucdavis.edu>
2024-02-08 15:50:42 -0800

    add input validation test; remove assert for login()

--- tests/test_projects.py
+++ tests/test_projects.py
@@ -1,3 +1,4 @@
+import pytest
 from flask import Flask
 from flask.testing import FlaskClient
 
@@ -6,14 +7,12 @@ from tests.conftest import AuthActions
 
 
 def test_create_project(client: FlaskClient, auth: AuthActions, app: Flask):
-    new_project_info = {
-        'owner': 1,
-        'name': 'new_project',
-        'category': 1,
-        'description': 'This is a test project!'}
+    new_project_info = {'owner': 1,
+                        'name': 'new_project',
+                        'category': 1,
+                        'description': 'This is a test project!'}
 
     token = auth.login()
-    assert token is not None
 
     response = client.post(
         '/projects/create',
@@ -36,3 +35,22 @@ def test_create_project(client: FlaskClient, auth: AuthActions, app: Flask):
 
         for key in new_project_info:
             assert projects[0][key] == new_project_info[key]
+
+
+@pytest.mark.parametrize(('owner', 'name', 'category', 'description', 'message'), (
+    (1, '', 1, '', b'Project name is required'),
+    (42, 'new_project', 1, '', b'Unauthorized'),
+    (1, 'new_project', -1, '', b'Invalid category'),
+    (1, 'Existing Project', 0, '', b'Project already exists')
+))
+def test_create_project_validate_input(
+        client: FlaskClient, auth: AuthActions, owner, name, category, description, message):
+    token = auth.login()
+
+    response = client.post(
+        '/projects/create',
+        headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'},
+        json={'owner': owner, 'name': name, 'category': category, 'description': description})
+
+    assert response.status_code == 400
+    assert message in response.data
