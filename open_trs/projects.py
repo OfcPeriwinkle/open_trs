9d8449793553be57d8f7587910c44267ea7e052e
OfcPeriwinkle <ajwald@ucdavis.edu>
2024-02-09 11:35:06 -0800

    add get_project()

--- open_trs/projects.py
+++ open_trs/projects.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import abort, Blueprint, jsonify
 
 import open_trs.db
 import open_trs.auth
@@ -23,3 +23,20 @@ def get_projects(user_id: int):
                           (user_id,)).fetchall()
 
     return jsonify({'projects': [dict(project) for project in projects]}), 200
+
+
+@bp.route('/<int:project_id>', methods=['GET'])
+@open_trs.auth.login_required
+def get_project(user_id: int, project_id: int):
+    db = open_trs.db.get_db()
+
+    project = db.execute(
+        'SELECT * FROM Projects WHERE id = ?', (project_id, )).fetchone()
+
+    if project is None:
+        raise open_trs.InvalidUsage(f'Project {project_id} does not exist', 404)
+
+    if project['owner'] != user_id:
+        raise open_trs.InvalidUsage('Forbidden', 403)
+
+    return jsonify({'project': dict(project)}), 200
