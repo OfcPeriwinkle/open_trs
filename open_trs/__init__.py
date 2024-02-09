050e180e6b1a880e3db4492068126d91fbd9c4db
OfcPeriwinkle <ajwald@ucdavis.edu>
2024-02-09 11:17:17 -0800

    add InvalidUsage exception and handler so invalid API calls

--- open_trs/__init__.py
+++ open_trs/__init__.py
@@ -1,11 +1,12 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify
 
 import open_trs.auth
 import open_trs.configs
 import open_trs.db
 import open_trs.projects
+
 
 CONFIGS = {
     'development': open_trs.configs.DevelopmentConfig,
@@ -13,6 +14,54 @@ CONFIGS = {
     'production': open_trs.configs.ProductionConfig,
     'ci': open_trs.configs.GitHubActionsConfig
 }
+
+
+class InvalidUsage(Exception):
+    """
+    Exception raised for invalid usage of an endpoint.
+    """
+
+    def __init__(self, message, status_code=None, payload=None):
+        """
+        Initialize a new InvalidUsage exception.
+
+        Args:
+            message (str): The message associated with the instance.
+            status_code (int, optional): The status code associated with the instance; defaults to None.
+            payload (dict, optional): The payload associated with the instance; defaults to None.
+        """
+        super().__init__()
+
+        self.message = message
+        self.status_code = status_code
+        self.payload = payload
+
+    def to_dict(self):
+        """
+        Convert the exception to a dictionary representation.
+
+        Returns:
+            A dictionary containing the exception details.
+        """
+
+        return {
+            'message': self.message,
+            'payload': self.payload,
+        }
+
+
+def handle_invalid_usage(exception: InvalidUsage):
+    """
+    Handle an InvalidUsage exception.
+
+    Args:
+        exception: The InvalidUsage exception to handle.
+
+    Returns:
+        A JSON response containing the exception details and status code.
+    """
+
+    return jsonify(exception.to_dict()), exception.status_code
 
 
 def create_app(testing: bool = False):
@@ -44,12 +93,14 @@ def create_app(testing: bool = False):
     except OSError:
         pass
 
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
+    # Register CLI commands and tear down functions
     open_trs.db.init_app(app)
+
+    # Register API blueprints
     app.register_blueprint(open_trs.auth.bp)
     app.register_blueprint(open_trs.projects.bp)
+
+    # Register error handlers
+    app.register_error_handler(InvalidUsage, handle_invalid_usage)
 
     return app
