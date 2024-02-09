3c5f7fbf1554282071b39849cb047659b32f9a18
OfcPeriwinkle <ajwald@ucdavis.edu>
2024-02-09 12:11:02 -0800

    update tests for project creation input validation

--- tests/test_projects.py
+++ tests/test_projects.py
@@ -7,12 +7,12 @@ from tests.conftest import AuthActions
 
 
 def test_create_project(client: FlaskClient, auth: AuthActions, app: Flask):
-    new_project_info = {'owner': 1,
-                        'name': 'new_project',
-                        'category': 1,
-                        'description': 'This is a test project!'}
-
     token = auth.login()
+    new_project_info = {
+        'name': 'new_project',
+        'category': 1,
+        'description': 'This is a test project!'
+    }
 
     response = client.post(
         '/projects/create',
@@ -37,20 +37,19 @@ def test_create_project(client: FlaskClient, auth: AuthActions, app: Flask):
             assert projects[0][key] == new_project_info[key]
 
 
-@pytest.mark.parametrize(('owner', 'name', 'category', 'description', 'message'), (
-    (1, '', 1, '', b'Project name is required'),
-    (42, 'new_project', 1, '', b'Unauthorized'),
-    (1, 'new_project', -1, '', b'Invalid category'),
-    (1, 'Existing Project', 0, '', b'Project already exists')
+@pytest.mark.parametrize(('name', 'category', 'description', 'message'), (
+    ('', 1, '', b'Project name is required'),
+    ('new_project', -1, '', b'Invalid category'),
+    ('Existing Project', 0, '', b'already exists')
 ))
 def test_create_project_validate_input(
-        client: FlaskClient, auth: AuthActions, owner, name, category, description, message):
+        client: FlaskClient, auth: AuthActions, name, category, description, message):
     token = auth.login()
 
     response = client.post(
         '/projects/create',
         headers={'Content-Type': 'application/json', 'Authorization': f'Bearer {token}'},
-        json={'owner': owner, 'name': name, 'category': category, 'description': description})
+        json={'name': name, 'category': category, 'description': description})
 
     assert response.status_code == 400
     assert message in response.data
